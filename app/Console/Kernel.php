bb1f5f0641f46374d89b2d6b20d2c6b21c85b04b
criticalthewizard <burroughszeb@me.com>
2018-12-28 14:26:38 -0600

    scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+      $schedule->command('streamer:featured')->everyThirtyMinutes();();
         // $schedule->command('inspire')
         //          ->hourly();
     }
