1ca67e430c5f84938f282083ab4c9e3e6f443ab7
criticalthewizard <burroughszeb@me.com>
2019-04-23 00:46:00 -0500

    hotfix social media per player

--- resources/views/components/player.blade.php
+++ resources/views/components/player.blade.php
@@ -7,7 +7,7 @@
       @endif
     </div>
     <div class="content flex-lb">
-      <div class="px-2 py-1 m-1 col-12 overlay">
+      <div class="px-2 py-1 m-1 col-12">
         <div class="flex-sbc">
           <strong style="font-size:1.2rem;">{{ $player->name }}</strong>
           <img src="{{$player->country_flag}}" alt="flag" height="15">
@@ -15,10 +15,18 @@
 
         <p class="m-0 py-1" style="font-size:0.9em;line-height:normal;">{{ $player->category->name }}</p>
         <div class="flex-lc fa-lg">
-          <i class="fab fa-twitter mr-2"></i>
-          <i class="fab fa-twitch mr-2"></i>
-          <i class="fab fa-instagram mr-2"></i>
-          <i class="fab fa-youtube mr-2"></i>
+          @if($player->twitter)
+            <a href="https://twitter.com/{{ $player->twitter }}"><i class="fab fa-twitter mr-2"></i></a>
+          @endif
+          @if($player->twitch)
+            <a href="https://twitch.tv/{{ $player->twitch }}"><i class="fab fa-twitch mr-2"></i></a>
+          @endif
+          @if($player->instagram)
+            <a href="https://instagram.com/{{ $player->instagram }}"><i class="fab fa-instagram mr-2"></i></a>
+          @endif
+          @if($player->youtube)
+            <a href="https://youtube.com/channel/{{ $player->youtube }}"><i class="fab fa-youtube mr-2"></i></a>
+          @endif
         </div>
       </div>
 
